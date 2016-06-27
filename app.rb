565bb4304d46e8c3cb4c2866fe0bb6a7c4c6d5b7
sr <skyrousse@gmail.com>
2016-06-27 11:21:25 -0700

    add a form to greeting_form page and capture the input from the form fields and pass those values to the greeting_card page

--- app.rb
+++ app.rb
@@ -16,3 +16,13 @@ end
 get('/favorite_photos') do
   erb(:favorite_photos)
 end
+
+get('/greeting_form') do
+  erb(:form)
+end
+
+get('/greeting_card') do
+  @sender = params.fetch('sender')
+  @recipient = params.fetch('recipient')
+  erb(:greeting_card)
+end
