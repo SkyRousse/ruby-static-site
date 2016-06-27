4c7b455b61a8e955c67037d71296450d44a5eb80
sr <skyrousse@gmail.com>
2016-06-27 11:03:05 -0700

    use variables for dynamic views

--- app.rb
+++ app.rb
@@ -2,26 +2,17 @@ require('sinatra')
 require('sinatra/reloader')
 
 get('/') do
+  @recipient = "Jessica"
+  @sender = "Jane"
   erb(:letter)
 end
 
 get('/australia') do
+  @recipient = "Eric"
+  @sender = "Doe"
   erb(:australia)
 end
 
 get('/favorite_photos') do
- "<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Hello Friend!</title>
-    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
-  </head>
-  <body>
-    <h1>Favorite Traveling Photos</h1>
-      <ul>
-        <li><img src='/img/arial-view-river.jpeg' class='img-responsive' alt='Nasa shot of river as seen from an arial view.'/></li>
-        <li><img src='/img/feet-jumping-in-puddle.jpeg' class='img-responsive' alt='Legs from knee down jumping over puddle.'/></li>
-      </ul>
-  </body>
-  </html>"
+  erb(:favorite_photos)
 end
